cdcf3630748d415b5b4cfb7fa67c3e74d0d29f20
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-16 10:27:28 -0400

    Scaffolded out required compnents, brought in axios dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Cache Stash</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Cache Stash</h1>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
