98c5cab9a40becc1a4dfe58441b58967cdb6e758
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-16 11:22:58 -0400

    Created Navbar, adjusted CSS color scheme. Brought in react router and created home and about pages

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return <div></div>;
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <Link to='/'>
+          {' '}
+          <i className={icon} /> {title}{' '}
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Cache Stash',
+  icon: 'fas fa-radiation-alt',
+};
--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>About This Application</h1>
+      <p className='my-1'>
+        {' '}
+        Application for managing your supplies during the apocalypse
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: 0.0.4</strong>
+      </p>
+    </div>
+  );
 };
 
 export default About;
