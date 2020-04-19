c2b10e991e6eb94df4f12c0a9468ab0f47cecbbe
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-19 18:00:29 -0400

    Added in login function to AuthState, added proper dispatch and cases to reducer. Added redirect away from sign in page if user is authenticated. Added in error alerts if a user doesnt fill in the sign fields correctly

--- client/src/context/auth/AuthState.jsx
+++ client/src/context/auth/AuthState.jsx
@@ -64,8 +64,28 @@ const AuthState = (props) => {
     }
   };
 
-  // login user
+  // Login
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.post('/api/auth', formData, config);
 
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
   // logout
 
   // clear errors
@@ -82,6 +102,7 @@ const AuthState = (props) => {
         register,
         clearErrors,
         loadUser,
+        login,
       }}>
       {props.children}
     </AuthContext.Provider>
