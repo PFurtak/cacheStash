d69272f81c4797a848bdbdc6b8a7c506f5de0897
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-18 22:51:00 -0400

    Brought in auth types, setup context api for auth state, brought in auth state to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,27 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import AuthState from './context/auth/AuthState';
 
 import CacheState from './context/cache/CacheState';
 
 const App = () => {
   return (
-    <CacheState>
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
+    <AuthState>
+      <CacheState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
-    </CacheState>
+        </Router>
+      </CacheState>
+    </AuthState>
   );
 };
 
