d7c686339962990c639e3658bc0f7cb7bd80044c
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-19 19:48:59 -0400

    created private route component, wrappedhome page of caches in private route. Verified functionality

--- client/src/components/routing/PrivateRoute.jsx
+++ client/src/components/routing/PrivateRoute.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
-const PrivateRoute = () => {
-  return <div></div>;
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
 };
 
 export default PrivateRoute;
